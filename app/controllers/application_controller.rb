65ae4f383757631ae7ab14d5bb73f19b446cc899
Kyle Talmage <ktalmage85@gmail.com>
2020-03-26 21:24:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,15 +38,17 @@ class ApplicationController < Sinatra::Base
     end
     redirect '/sessions/login'
   end
+  
+  get '/users/home' do
+
+    @user = User.find(session[:user_id])
+    erb :'/users/home'
+  end
 
   get '/sessions/logout' do
     session.clear
     redirect '/'
   end
 
-  get '/users/home' do
-
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
-  end
+  
 end
